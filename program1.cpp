aaf097e1d510ec4870faf29c74d1467bab53c88d
earnold14 <ema190000@utdallas.edu>
2022-02-22 20:06:58 -0600

    Add existing file

---
+++ program1.cpp
@@ -0,0 +1,19 @@
+/*
+ * Ethan Arnold
+ * Project 1 - program1.cpp
+ * 
+ * Tests the indexed load instructions.
+ * Prints two tables, one of A-Z, the other of 1-10.
+ *
+**/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "hello" << endl;
+  return 0;
+
+
+  
+}
